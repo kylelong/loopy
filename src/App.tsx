2581f8cfcddba9f86e2cbb73ae0c1b367b55564d
kyle long <kylel95@vt.edu>
2023-08-17 03:02:37 -0400

    add sections to describe site

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import "./App.css";
 import styled from "styled-components";
 import music from "./assets/music.svg";
+import heart from "./assets/heart.svg";
+import globe from "./assets/globeEuropeAfrica.svg";
 // import Share from "./Share";
-// import heart from "./assets/heart.svg";
+import magnifyingGlass from "./assets/magnifyingGlass.svg";
+import play from "./assets/play.svg";
 import Carousel from "./Carousel";
 
 export const Saying = styled.div`
@@ -14,6 +17,17 @@ export const Saying = styled.div`
   color: rgb(17, 17, 17);
   max-width: 800px;
   width: 100%;
+`;
+
+export const Listening = styled.div`
+  font-family: "Helvetica Neue", sans-serif;
+  font-weight: bold;
+  font-size: 23px;
+  margin-top: 18px;
+  color: rgb(17, 17, 17);
+  max-width: 800px;
+  width: 100%;
+  letter-spacing: -0.5px;
 `;
 export const Header = styled.div`
   display: flex;
@@ -106,7 +120,7 @@ export const MenuItems = styled.div`
 
 export const MenuItem = styled.div`
   font-family: "Helvetica Neue", sans-serif;
-  color: rgb(102, 102, 102);
+  color: #9ca3af;
   font-weight: bold;
 
   &:hover {
@@ -115,6 +129,68 @@ export const MenuItem = styled.div`
   }
 `;
 
+export const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 560px;
+  width: 100%;
+  border-radius: 0.75rem;
+
+  margin: 24px;
+  position: relative;
+  left: 14px;
+  @media (max-width: 600px) {
+    left: unset;
+  }
+`;
+
+export const SectionHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const SectionHeaderText = styled.div`
+  font-family: "Helvetica Neue", sans-serif;
+  font-weight: bold;
+  font-size: 24px;
+  margin-top: 18px;
+  color: rgb(93, 93, 255);
+  max-width: 800px;
+  width: 100%;
+  letter-spacing: -0.5px;
+`;
+export const Heart = styled.img`
+  width: 1.7rem;
+  margin-right: 12px;
+  position: relative;
+  top: 11px;
+`;
+export const Globe = styled.img`
+  width: 1.7rem;
+  margin-right: 12px;
+  position: relative;
+  top: 9px;
+`;
+
+export const MagnifyingGlass = styled.img`
+  width: 1.7rem;
+  margin-right: 12px;
+  position: relative;
+  top: 11px;
+`;
+
+export const Play = styled.img`
+  width: 1.7rem;
+  margin-right: 12px;
+  position: relative;
+  top: 11px;
+`;
+
+export const Description = styled.div`
+  font-family: "Helvetica Neue", sans-serif;
+  font-weight: 380;
+  margin-bottom: 6px;
+`;
 // https://soundcloud.com/thewebbyawards/the-juan-maclean-happy-house?si=e558539c189f40b691a6aaca00dd512e&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing
 // https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F16959100&show_artwork=true
 
@@ -144,8 +220,58 @@ function App() {
             Discover songs you love from people that already love them.
           </Saying>
           <SignUpBtn>Sign up</SignUpBtn>
+          <div style={{display: "flex", flexDirection: "row"}}>
+            <Globe src={globe} />
+            <Listening>
+              What are people listening to around the world?
+            </Listening>
+          </div>
         </Header>
+
         <Carousel />
+        <Section>
+          <SectionHeader>
+            <Heart src={heart} />
+            <SectionHeaderText>
+              share the songs you really love
+            </SectionHeaderText>
+          </SectionHeader>
+          <Description>
+            upload a link to your{" "}
+            <b>
+              <i>all-time</i>
+            </b>{" "}
+            favorite songs.
+          </Description>
+          <Description>
+            the kind of songs when you first heard it you listened to it on
+            repeat for 3 days straight.
+          </Description>
+        </Section>
+
+        <Section>
+          <SectionHeader>
+            <MagnifyingGlass src={magnifyingGlass} />
+            <SectionHeaderText>explore songs from 50+ genres</SectionHeaderText>
+          </SectionHeader>
+
+          <Description>
+            different genres from all kinds of artist, from all around the
+            world.
+          </Description>
+          <Description>from house to rap, we got range covered</Description>
+        </Section>
+
+        <Section>
+          <SectionHeader>
+            <Play src={play} />
+            <SectionHeaderText>find your new favorite songs</SectionHeaderText>
+          </SectionHeader>
+          <Description>
+            if someone else really loves a song, there is a chance you do to{" "}
+            <span>🤞🏾</span>
+          </Description>
+        </Section>
       </Container>
 
       {/* <Share /> */}

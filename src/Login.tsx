f2b605dfd9282975f70142ab55eb86e381a60979
kyle long <kylel95@vt.edu>
2023-08-18 16:00:29 -0400

    redesign Login

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,15 +1,41 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import styled from "styled-components";
+import LoopyLogo from "./LoopyLogo";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 24px;
+`;
 
 export const LoginForm = styled.form`
   display: flex;
   flex-direction: column;
+  max-width: 400px;
+  width: 100%;
+  height: 430px;
+  border-radius: 0.75rem;
+  background-color: #f8fafc;
+  box-shadow: 0 10px 15px -3px rgb(203, 213, 225),
+    0 4px 6px -4px rgb(203, 213, 225);
+  padding: 42px;
   margin: 24px;
 `;
 
+export const HeaderText = styled.div`
+  font-family: "Helvetica Neue", sans-serif;
+  font-size: 24px;
+  font-weight: bold;
+  letter-spacing: -1px;
+  color: rgb(17, 17, 17);
+  max-width: 800px;
+  width: 100%;
+  margin-bottom: 12px;
+`;
+
 export const InputBox = styled.input`
-  margin-bottom: 10px;
   width: 256px;
   height: 40px;
   font-family: sans-serif;
@@ -34,6 +60,7 @@ export const LoginButton = styled.button`
   font-family: sans-serif;
   font-size: 16px;
   font-weight: bold;
+  margin-top: 6px;
   margin-bottom: 15px;
   text-align: center;
   opacity: 0.8;
@@ -43,15 +70,55 @@ export const LoginButton = styled.button`
   }
 `;
 
+export const Label = styled.label`
+  font-family: "Helvetica Neue", sans-serif;
+  font-size: 14px;
+  margin-bottom: 6px;
+`;
+
+export const NoAccount = styled.div`
+  font-family: sans-serif;
+  font-size: 14px;
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+`;
+export const linkStyle = {
+  textDecoration: "none",
+  fontFamily: "Helvetica Neue, sans-serif",
+  color: "rgb(93, 93, 255)",
+};
 const Login = () => {
   return (
-    <LoginForm>
-      <div>welcome back</div>
-      <InputBox placeholder="Email" />
+    <Container>
+      <LoginForm>
+        <div style={{position: "relative", right: "24px"}}>
+          <LoopyLogo />
+        </div>
+        <HeaderText>Sign in to Loopy</HeaderText>
+        <InputContainer>
+          <Label>Email</Label>
+          <InputBox />
+        </InputContainer>
 
-      <InputBox placeholder="Password" />
-      <LoginButton>Login</LoginButton>
-    </LoginForm>
+        <InputContainer>
+          <Label>Password</Label>
+          <InputBox type="password" />
+        </InputContainer>
+        <InputContainer>
+          <LoginButton>Login</LoginButton>
+        </InputContainer>
+        <NoAccount>
+          Don't have an account?{" "}
+          <Link to="/signup" style={linkStyle}>
+            Sign up
+          </Link>
+        </NoAccount>
+      </LoginForm>
+    </Container>
   );
 };
 
---
+++ src/LoopyLogo.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+import music from "./assets/music.svg";
+
+export const LogoHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  @media (max-width: 350px) {
+    position: relative;
+    right: 15px;
+  }
+`;
+export const Logo = styled.div`
+  font-weight: bold;
+  color: rgb(17, 17, 17);
+  font-size: 22px;
+  font-family: "Helvetica Neue", sans-serif;
+  margin: 24px;
+`;
+
+export const Music = styled.img`
+  width: 1.5rem;
+  height: 1.5rem;
+  position: relative;
+  top: 27px;
+  right: 19px;
+`;
+
+const LoopyLogo = () => {
+  return (
+    <>
+      <LogoHeader>
+        <Logo>loopy</Logo>
+        <Music src={music} />
+      </LogoHeader>
+    </>
+  );
+};
+
+export default LoopyLogo;

f02879ff1418385b3fa3ba357ee486bfd3d70b6f
kyle long <kylel95@vt.edu>
2023-09-05 01:47:37 -0400

    update .gitignore

---
+++ client/src/firebase-config.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "firebase/app";
+// import {getAnalytics} from "firebase/analytics";
+import {getAuth} from "firebase/auth";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+// loopy
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCQ99IeCWJ5mQ0ay3cP_tyyG9rhxDZwebA",
+//   authDomain: "loopy-8c163.firebaseapp.com",
+//   projectId: "loopy-8c163",
+//   storageBucket: "loopy-8c163.appspot.com",
+//   messagingSenderId: "648426823123",
+//   appId: "1:648426823123:web:c0c234e7199a473daf2326",
+//   measurementId: "G-162296BMWG",
+// };
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);

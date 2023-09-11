91fce3a234549a2c9cc7237266e0bd98df2007ad
kyle long <kylel95@vt.edu>
2023-09-11 01:00:31 -0400

    comment axios call out

--- server/index.js
+++ server/index.js
@@ -8,7 +8,6 @@ require("dotenv").config({
 const express = require("express");
 const config = require("./config");
 const bodyParser = require("body-parser");
-const axios = require("axios");
 const app = express();
 const corsOptions = {
   origin: true,
@@ -32,25 +31,25 @@ app.get("/", (req, res) => {
 });
 // TODO:
 
-app.get("/spotify", async (req, res) => {
-  try {
-    // Get the Spotify URL from the query parameter
-    const {url} = req.query;
-    const apiUrl = `https://open.spotify.com/oembed?url=${url}&format=json`;
-    const response = await axios.get(apiUrl);
-    // Make the Axios GET request to Spotify
+// app.get("/spotify", async (req, res) => {
+//   try {
+//     // Get the Spotify URL from the query parameter
+//     const {url} = req.query;
+//     const apiUrl = `https://open.spotify.com/oembed?url=${url}&format=json`;
+//     const response = await axios.get(apiUrl);
+//     // Make the Axios GET request to Spotify
 
-    // Set appropriate CORS headers
-    // res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000"); // Replace with your front-end's origin
-    // res.setHeader("Access-Control-Allow-Credentials", "true");
+//     // Set appropriate CORS headers
+//     // res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000"); // Replace with your front-end's origin
+//     // res.setHeader("Access-Control-Allow-Credentials", "true");
 
-    // Send the data as JSON in the response
-    res.json(response.data.html);
-  } catch (error) {
-    console.error("Error making Axios GET request:", error);
-    res.status(500).json({error: "Internal Server Error"});
-  }
-});
+//     // Send the data as JSON in the response
+//     res.json(response.data.html);
+//   } catch (error) {
+//     console.error("Error making Axios GET request:", error);
+//     res.status(500).json({error: "Internal Server Error"});
+//   }
+// });
 
 app.post("/register", async (req, res) => {
   try {
